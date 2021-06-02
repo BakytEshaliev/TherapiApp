474783da24f09d520228a2268d9978c6be508b87
Ataibek Isakov <atayisakov558@gmail.com>
2021-06-02 15:48:34 +0200

    refactor views

--- app/src/main/java/com/example/therapiapp/MainActivity.java
+++ app/src/main/java/com/example/therapiapp/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.activity_anmeldung_1);
 
         startseiteIntent = new Intent(MainActivity.this, Startseite.class);
         chatbotIntent = new Intent(MainActivity.this, ChatBot.class);
