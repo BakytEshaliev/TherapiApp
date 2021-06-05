56edec91bed3d676f34cecac7e409d7b7ebed95f
Bekzhan Satarov <satarovbekzhan@gmail.com>
2021-06-06 01:33:45 +0200

    Some refactroring of a chatbot functionality

--- app/src/main/java/com/example/therapiapp/ChatBot.java
+++ app/src/main/java/com/example/therapiapp/ChatBot.java
@@ -1,8 +1,11 @@
 package com.example.therapiapp;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import androidx.appcompat.app.AlertDialog;
 
 public class ChatBot extends BaseActivity {
 
@@ -12,31 +15,52 @@ public class ChatBot extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (ChatBot.chatBotPrivacyPolicyAcceptedForSave) {
-            if (ChatBot.chatBotPrivacyPolicyAcceptedForUse) {
-                setContentView(R.layout.activity_chat_bot);
-            } else setContentView(R.layout.activity_chat_bot_privacy_use);
-        } else setContentView(R.layout.activity_chat_bot_privacy_save);
+        setContentView(R.layout.activity_chat_bot);
         initToolbar();
+
+        if (!ChatBot.chatBotPrivacyPolicyAcceptedForSave) askForDataSavePermission();
+        if (!ChatBot.chatBotPrivacyPolicyAcceptedForUse) askForDataUsePermission();
     }
 
-    public void acceptPrivacyPolicySave(View v) {
-        ChatBot.chatBotPrivacyPolicyAcceptedForSave = true;
-        Intent intent = new Intent(this, ChatBot.class);
-        this.startActivity(intent);
+    private void askForDataSavePermission() {
+        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which){
+                    case DialogInterface.BUTTON_POSITIVE:
+                        ChatBot.chatBotPrivacyPolicyAcceptedForSave = true;
+                        break;
+
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        finish();
+                        break;
+                }
+            }
+        };
+        String message = "Bitte lies dir die folgende Hinweise genau durch und stimme dem nur zu, wenn du das möchtest. \n\nStimmst Du zu, dass deine Daten auf einem Server gespeichert werden, damit der Chatbotdementsprechend auf deine Antworten reagieren kann? Diese Einstellung kann jederzeit unter deinen Einstellungen aktiviert oder deaktiviert werden.";
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Hinweis").setMessage(message).setPositiveButton("Ja", dialogClickListener)
+                .setNegativeButton("Nein", dialogClickListener).show();
     }
 
-    public void declinePrivacyPolicySave(View v) {
-        finish();
-    }
+    private void askForDataUsePermission() {
+        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which){
+                    case DialogInterface.BUTTON_POSITIVE:
+                        ChatBot.chatBotPrivacyPolicyAcceptedForUse = true;
+                        break;
 
-    public void acceptPrivacyPolicyUse(View v) {
-        ChatBot.chatBotPrivacyPolicyAcceptedForUse = true;
-        Intent intent = new Intent(this, ChatBot.class);
-        this.startActivity(intent);
-    }
-
-    public void declinePrivacyPolicyUse(View v) {
-        finish();
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        finish();
+                        break;
+                }
+            }
+        };
+        String message = "Bitte lies dir die folgende Hinweise genau durch und stimme dem nur zu, wenn du das möchtest. \n\nStimmst Du zu, dass der von dir angegebene Aurzt auf deine Daten zugreifen kann und diese zur Unterstutzung deiner Behandlung nutzen darf?";
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Hinweis").setMessage(message).setPositiveButton("Ja", dialogClickListener)
+                .setNegativeButton("Nein", dialogClickListener).show();
     }
 }
