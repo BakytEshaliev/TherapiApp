719e43ed0cff11608d1513483c1b35f28a4304a4
Ataibek Isakov <atayisakov558@gmail.com>
2021-06-17 17:13:46 +0200

    implemented wiki search function

--- app/src/main/java/com/example/therapiapp/Wiki.java
+++ app/src/main/java/com/example/therapiapp/Wiki.java
@@ -1,21 +1,39 @@
 package com.example.therapiapp;
 
+import android.app.SearchManager;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.net.URLEncoder;
+
 public class Wiki extends BaseActivity {
+
+    private EditText searchEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wiki);
         initToolbar("Wiki");
-
+        searchEditText = findViewById(R.id.wikiSearchEditText);
         RecyclerView recyclerView = findViewById(R.id.wiki_recycler);
         WikiAdapter wikiAdapter = new WikiAdapter(this);
         recyclerView.setAdapter(wikiAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    public void handleSearch(View view) {
+        String query = searchEditText.getText().toString();
+        Uri uri = Uri.parse("https://en.wikipedia.org/wiki/"+query);
+        Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+        startActivity(intent);
     }
 }
