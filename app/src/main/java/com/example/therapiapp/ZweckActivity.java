3c57bef05e7e76ff2159cb6e0081c16a6221dee6
Ataibek Isakov <atayisakov558@gmail.com>
2021-07-07 19:46:03 +0200

    zweck,notfall,anmeldungerflg

--- app/src/main/java/com/example/therapiapp/ZweckActivity.java
+++ app/src/main/java/com/example/therapiapp/ZweckActivity.java
@@ -5,17 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class ZweckActivity extends BaseActivity {
-
+    private CheckBox zweckZB1;
+    private CheckBox zweckZB2;
+    private CheckBox zweckZB3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appnutzungszweck);
+        zweckZB1 = findViewById(R.id.zweckCheckBox1);
+        zweckZB2 = findViewById(R.id.zweckCheckBox2);
+        zweckZB3 = findViewById(R.id.zweckCheckBox3);
     }
     public void weiterClick(View view){
-        Intent intent = new Intent(this,Startseite.class);
-        startActivity(intent);
-        finish();
+        if (isSelected()){
+            Intent intent = new Intent(this,Startseite.class);
+            startActivity(intent);
+            finish();
+        }else Toast.makeText(this,"Wählen Sie mindestens ein Zweck!",Toast.LENGTH_SHORT).show();
+    }
+
+    private boolean isSelected(){
+        return zweckZB1.isChecked() || zweckZB2.isChecked() || zweckZB3.isChecked();
     }
 }
